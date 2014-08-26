a5cdd7da49b233ab9e5c35979a7fac4b7f7b9124
kparks29 <ken@awesome>
2014-08-25 22:11:35 -0700

    styled layout for page

--- js/main.js
+++ js/main.js
@@ -9,11 +9,87 @@ app.controller("ibcCtrl", function($scope, $sce){
   $scope.videos = [
     {
       url: "http://www.youtube.com/embed/XS6ysDFTbLU",
-      title: "Bill Gates ALS Ice Bucket Challenge"
+      title: "Bill Gates ALS Ice Bucket Challenge",
+      comments: [
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        },
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        },
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        },
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        }
+      ]
     },
     {
       url: "http://www.youtube.com/embed/-tnywhcDaAc",
-      title: "Ryan Seacrest ALS Ice Bucket Challenge"
+      title: "Ryan Seacrest ALS Ice Bucket Challenge",
+      comments: [
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        },
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        },
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        },
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        }
+      ]
+    },
+    {
+      url: "http://www.youtube.com/embed/XS6ysDFTbLU",
+      title: "Bill Gates ALS Ice Bucket Challenge",
+      comments: [
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        }
+      ]
+    },
+    {
+      url: "http://www.youtube.com/embed/-tnywhcDaAc",
+      title: "Ryan Seacrest ALS Ice Bucket Challenge",
+      comments: [
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        }
+      ]
+    },
+    {
+      url: "http://www.youtube.com/embed/XS6ysDFTbLU",
+      title: "Bill Gates ALS Ice Bucket Challenge",
+      comments: [
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        }
+      ]
+    },
+    {
+      url: "http://www.youtube.com/embed/-tnywhcDaAc",
+      title: "Ryan Seacrest ALS Ice Bucket Challenge",
+      comments: [
+        {
+          name: "Someone",
+          text: "wow what an amazing video! \nLet me click on your ad now"
+        }
+      ]
     }
   ]
 
