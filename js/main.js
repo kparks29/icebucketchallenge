eaf47f287f4c737184424ee6a4bd6d4a6cf9ad0e
RasPhilCo <Ras@PhilCo>
2014-09-02 18:52:59 -0400

    Add rails api call & get ready for heroku

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-var app = angular.module("ibcApp", ['ngRoute', 'ngSanitize', 'ngDisqus', 'firebase']);
+var app = angular.module("ibcApp", ['ngRoute', 'ngSanitize', 'ngDisqus']);
+var host = 'http://iceblastapi.herokuapp.com';
 
 app.config(function($disqusProvider, $locationProvider, $routeProvider) {
   $disqusProvider.setShortname('ibcblast'); // Configure the disqus shortname
-  $locationProvider.hashPrefix('!');                 // Disqus needs hashbang in urls. If you are using pushstate then no need for this.
+  $locationProvider.hashPrefix('!');        // Disqus needs hashbang in urls. If you are using pushstate then no need for this.
   // Configure your amazing routes
   $routeProvider.when('/v/:id', {
     templateUrl : 'partials/searchTpl.html',
@@ -15,114 +16,12 @@ app.config(function($disqusProvider, $locationProvider, $routeProvider) {
   });
 });
 
-app.controller("ibcCtrl", function($scope, $sce, $routeParams, $firebase){
-  var ref = new Firebase("https://icebuckettube.firebaseio.com/");
-  var sync = $firebase(ref);
-  var syncObject = sync.$asObject();
-  syncObject.$bindTo($scope, "data");
+app.controller("ibcCtrl", function($scope, $sce, $routeParams, $http){
+  $http.get(host + '/api/v1/videos.json?page=1&per_page=15').success(function(videos) {
+    $scope.videos = videos['results'];
+  });
   $scope.route = $routeParams;
   $scope.trustSrc = function(src) {
     return $sce.trustAsResourceUrl(src);
   };
-
-  $scope.videos = [
-    {
-      url: "http://www.youtube.com/embed/XS6ysDFTbLU",
-      ytId: "XS6ysDFTbLU",
-      title: "Bill Gates ALS Ice Bucket Challenge",
-      popularity: 1,
-      comments: [
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        },
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        },
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        },
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        }
-      ]
-    },
-    {
-      url: "http://www.youtube.com/embed/-tnywhcDaAc",
-      ytId: "-tnywhcDaAc",
-      title: "Ryan Seacrest ALS Ice Bucket Challenge",
-      popularity: 3,
-      comments: [
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        },
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        },
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        },
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        }
-      ]
-    },
-    {
-      url: "http://www.youtube.com/embed/XS6ysDFTbLU",
-      ytId: "XS6ysDFTbLU",
-      title: "Bill Gates ALS Ice Bucket Challenge",
-      popularity: 7,
-      comments: [
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        }
-      ]
-    },
-    {
-      url: "http://www.youtube.com/embed/-tnywhcDaAc",
-      ytId: "-tnywhcDaAc",
-      title: "Ryan Seacrest ALS Ice Bucket Challenge",
-      popularity: 2,
-      comments: [
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        }
-      ]
-    },
-    {
-      url: "http://www.youtube.com/embed/XS6ysDFTbLU",
-      ytId: "XS6ysDFTbLU",
-      title: "Bill Gates ALS Ice Bucket Challenge",
-      popularity: 1,
-      comments: [
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        }
-      ]
-    },
-    {
-      url: "http://www.youtube.com/embed/-tnywhcDaAc",
-      ytId: "-tnywhcDaAc",
-      title: "Ryan Seacrest ALS Ice Bucket Challenge",
-      popularity: 0,
-      comments: [
-        {
-          name: "Someone",
-          text: "wow what an amazing video! \nLet me click on your ad now"
-        }
-      ]
-    }
-  ]
 });
-
-
