b206777638247af0c338c553f85f0272a944ccba
RasPhilCo <Ras@PhilCo>
2014-09-04 00:50:17 -0400

    Add up/down vote

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var app = angular.module("ibcApp", ['ngRoute', 'ngSanitize', 'ngDisqus']);
-var host = 'http://iceblastapi.herokuapp.com';
+var app = angular.module("ibcApp", ['ngRoute', 'ngSanitize', 'ngDisqus', 'truncFilter']);
+// var host = 'http://iceblastapi.herokuapp.com';
+var host = 'http://localhost:3000';
 
 app.config(function($disqusProvider, $locationProvider, $routeProvider) {
   $disqusProvider.setShortname('ibcblast'); // Configure the disqus shortname
@@ -32,4 +33,28 @@ app.controller("ibcCtrl", function($scope, $sce, $routeParams, $http){
     });
   };
 
+  $scope.update = function(video) {
+    $http.put(host + "/api/v1/videos/" + video.yt_id + ".json?vote=" + video.vote);
+    if (video.vote == "up"){
+      video.popularity++;}
+    else if (video.vote == "down"){
+      video.popularity--;}
+  };
+
 });
+
+angular.module('truncFilter', []).
+  filter('truncate', function () {
+        return function (text, length, end) {
+            if (isNaN(length))
+                length = 10;
+            if (end === undefined)
+                end = "...";
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+        };
+    });
